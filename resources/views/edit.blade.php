c6bcd7fe1fad95be44b05126a3a1ebc7facbfc25
Andrei Zhiburtovich <andzib@gmail.com>
2023-03-06 21:59:12 +0300

    methods (add, create, edit, update) + Route-Model Binding to TasksController were added

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,10 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            //header
+        </h2>
+    </x-slot>
+
+    //body
+
+</x-app-layout>
