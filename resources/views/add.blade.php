acb0292d75adbcaf96b49f71be3c68ffc8d54f5f
Andrei Zhiburtovich <andzib@gmail.com>
2023-03-11 21:59:22 +0300

    Update templates dashboard.blade.php, add.blade.php, edit.blade.php (css changes - color, padding, margin)

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
 
                 <form method="POST" action="/task">
 
@@ -19,7 +19,7 @@
                     </div>
 
                     <div class="form-group">
-                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Add Task</button>
+                        <button type="submit" class="bg-indigo-500  bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 mt-2 rounded">Add Task</button>
 
                     {{ csrf_field() }}
                 </form>
--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-6">
 
                 <form method="POST" action="/task/{{ $task->id }}">
 
@@ -19,7 +19,7 @@
                     </div>
 
                     <div class="form-group">
-                        <button type="submit" name="update" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Update task</button>
+                        <button type="submit" name="update" class="bg-indigo-500  bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-4 mt-2 rounded">Update task</button>
                     </div>
                     {{ csrf_field() }}
                 </form>
