cb5c655e6eb5ba4f27a0a8a5b8c1a2c503e81b57
Andrei Zhiburtovich <andzib@gmail.com>
2023-03-27 22:43:20 +0300

    Changed some files because of the plugin "IDE helper" was added.

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -7,7 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 
 /**
  * Class Task
+ *
  * @package App\Models
+ * @property int $id
+ * @property string $description
+ * @property int $marked
+ * @property int $user_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\User $user
+ * @method static \Illuminate\Database\Eloquent\Builder|Task newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Task newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Task query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Task whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Task whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Task whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Task whereMarked($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Task whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Task whereUserId($value)
+ * @mixin \Eloquent
  */
 class Task extends Model
 {
